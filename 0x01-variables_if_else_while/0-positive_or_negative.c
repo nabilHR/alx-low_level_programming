356772cb7af3286c1a6738a7aee1399034588e68
nabilHR <45454081+nabilHR@users.noreply.github.com>
2023-09-10 20:37:55 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,6 +15,6 @@ int main(void)
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n > 0)
-                	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	return (0);
 }
